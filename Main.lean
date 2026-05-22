1e27038515bd2bc9ae7db19f44996195d90e2f61
Greg Shuflin <greg@everydayimshuflin.com>
2026-05-22 01:54:03 -0700

    Use a macro to generate arguments

--- Main.lean
+++ Main.lean
@@ -2,16 +2,16 @@ import Climax
 
 def cliArgs: Parser := Id.run do
   let mut parser := Parser.new "climax-example"
-  parser := parser.addArgument <| Argument.flag "chutney" "Generate some chutney"
-  parser := parser.addArgument <| Argument.flag "bananas" "Go absolutely bananas"
-  parser := parser.addArgument <| Argument.flag "arch" "Architecture"
+  parser := parser.addArgument (argument "-c" "--chutney" "Generate some chutney")
+  parser := parser.addArgument (argument "-b" "--bananas" "Go absolutely bananas")
+  parser := parser.addArgument (argument "--arch" "Architecture")
 
   return parser
 
 def main (args: List String): IO Unit := do
 
   let parser := cliArgs
-  let cliMatches <- parser.run args
+  let _cliMatches <- parser.run args
 
   IO.println s!"This program demonstrates a simple use of the climax command line argument parsing library. Try passing in --help"
 

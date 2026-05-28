ecf376f3af16305dd8671c58b3eba8323e9ef785
Greg Shuflin <greg@everydayimshuflin.com>
2026-05-28 03:08:02 -0700

    Improving example

--- Main.lean
+++ Main.lean
@@ -3,16 +3,23 @@ import Climax
 def cliArgs: ArgParser :=
   let argParser := ArgParser.new "climax-example"
   arguments argParser
-    | "-c" "--chutney" "Generate some chutney"
-    | "-b" "--bananas" "Go absolutely bananas"
-    | "--arch" "Architecture"
+    | "-f" "--food" "Enter the name of a foodstuff"
 
 def main (args: List String): IO Unit := do
 
   let parser := cliArgs
-  let _cliMatches <- parser.run args
+  let cliMatches <- parser.run args
 
-  IO.println s!"This program demonstrates a simple use of the climax command line argument parsing library. Try passing in --help"
+  if args.isEmpty then
+    IO.println s!"This program demonstrates a simple use of the climax command line argument parsing library. Try passing in --help"
+    IO.Process.exit 1
+
+
+  if let some m := cliMatches.getMatch? "food" then
+    let food := m.value
+    IO.println s!"Entered a food value: {food}"
+
+
 
 
 
